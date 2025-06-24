b651dbf47daa53c3c4476406a61190087e7f9409
VaishnaviKadam07-a <vaishnavikadam7105@gmail.com>
2025-06-24 16:27:40 +0530

    Add files via upload

---
+++ src/page/Chatpage.jsx
@@ -0,0 +1,13 @@
+// pages/ChatPage.jsx
+import React from "react";
+import Chatbot from "../components/Chatbot";
+
+const ChatPage = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <Chatbot />
+    </div>
+  );
+};
+
+export default ChatPage;
